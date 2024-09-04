e8616e04f93a8ed36d311c169baf1cd681fccc29
chrissundal <christoffersj@hotmail.com>
2024-09-04 14:20:17 +0200

    skrevet om til mvc

--- script.js
+++ script.js
@@ -1,23 +1,50 @@
-let lightSwitch = false;
+//Model
 
-        function toggleLights() {
-            lightSwitch = !lightSwitch;
-            
-            if (lightSwitch == false) {
-                turnOff();
-            } else {
-                turnOn();
-            }
-        }
-        function turnOn() {
-            document.getElementById('bulb').style.backgroundColor = "yellow";
-            document.getElementById('off').innerHTML = "";
-            document.getElementById('body').style.backgroundColor = "white";
-            document.getElementById('on').innerHTML = "ON";
-        }
-        function turnOff() {
-            document.getElementById('bulb').style.backgroundColor = "gray";
-            document.getElementById('off').innerHTML = "OFF";
-            document.getElementById('body').style.backgroundColor = "black";
-            document.getElementById('on').innerHTML = "";
-        }
+let lightSwitch = false;
+let lysMessage = '<br/><br/><br/><br/>OFF';
+const app = document.getElementById('app');
+
+//View
+
+updateView();
+function updateView() {
+    let html = /*HTML*/`
+        <div class="lys">
+            <div class="sokkel"></div>
+            ${velgBulb()}
+    </div>
+    <div class="bryter">
+        <div class="knapp" onclick="toggleLights()">
+            <div class="on">${lysMessage}</div>
+        </div>
+    </div>
+    `;
+    app.innerHTML = html;
+}
+function velgBulb() {
+    if (lightSwitch == false) return `<div class="bulb"></div>`
+    return `<div style="background-color: yellow; "class="bulb"></div>`
+}
+
+//controller
+
+function toggleLights() {
+    lightSwitch = !lightSwitch;
+
+    if (lightSwitch == false) {
+        turnOff();
+    } else {
+        turnOn();
+    }
+    updateView();
+}
+function turnOn() {
+    document.body.style.backgroundColor = "white";
+    lysMessage = `ON`;
+    updateView();
+}
+function turnOff() {
+    document.body.style.backgroundColor = "black";
+    lysMessage = `<br/><br/><br/><br/>OFF`;
+    updateView();
+}
